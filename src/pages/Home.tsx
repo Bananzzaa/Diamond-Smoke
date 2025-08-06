68acb186314e3f4363f4d09c8a5b0eb524580d92
Bananzzaa <makssalihov2000@gmail.com>
2025-08-07 01:25:07 +0300

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Добро пожаловать в Diamond Smoke</h1>
+      <p>Каталог табачной продукции будет здесь</p>
+    </div>
+  );
+};
+
+export default Home;
